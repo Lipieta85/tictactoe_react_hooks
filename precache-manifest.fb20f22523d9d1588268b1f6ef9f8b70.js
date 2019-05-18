a1c594d2bcff4deb0d3e57582d79b63678d0162c
Rafał Lipieta <cwgflojdid@wp.pl>
2019-05-18 16:58:37 +0200

    Updates

---
+++ precache-manifest.fb20f22523d9d1588268b1f6ef9f8b70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "65be5c45496aa11ba3d2",
+    "url": "/tictactoe_react_hooks/static/js/runtime~main.97334089.js"
+  },
+  {
+    "revision": "1127ab3635ffd7efed42",
+    "url": "/tictactoe_react_hooks/static/js/main.f5da4f2c.chunk.js"
+  },
+  {
+    "revision": "caa9a7fa08bf2c0d73b8",
+    "url": "/tictactoe_react_hooks/static/js/2.f0910273.chunk.js"
+  },
+  {
+    "revision": "1127ab3635ffd7efed42",
+    "url": "/tictactoe_react_hooks/static/css/main.d0a02b5c.chunk.css"
+  },
+  {
+    "revision": "94a84cb5d7a756882b7aaa4ef7beedc1",
+    "url": "/tictactoe_react_hooks/index.html"
+  }
+];
